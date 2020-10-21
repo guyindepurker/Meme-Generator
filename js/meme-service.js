f2d13b8dd8c90351c000cbcb8cdce8610300426c
Guy Indepurker <guy235@gmail.com>
2020-10-21 12:18:43 +0300

    crate basic service and canvas w,h

---
+++ js/meme-service.js
@@ -0,0 +1,14 @@
+var gKeywords = {'happy':12,'funny':1};
+
+var gImgs = [{id:1,url:'imgs/meme-imgs/1.jpg',keywords:['happy']}];
+
+var gMeme = {
+    selectedImgId:1,
+    selectedLineIdx:0,
+    line:[{
+        txt:'I never eat Falafel',
+        size:20,
+        align:'left',
+        color:'red'
+    }]
+};
