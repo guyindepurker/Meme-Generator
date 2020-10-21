6bb27948dbcce1f1468b4202d2464ecb1563129e
Guy Indepurker <guy235@gmail.com>
2020-10-21 14:20:07 +0300

    render mem+with txt

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,4 +1,38 @@
 'use strict';
-console.log('hey');
+var gCanvas;
+var gCtx;
+function init(){
+    gCanvas = document.querySelector('#meme-canvas');
+    gCtx = gCanvas.getContext('2d');
+    renderMeme()
+}
+function hendleEvent(ev){
+    console.log(ev);
+}
+function renderMeme(){
+    var meme = getMeme();
+    var img = findImgById(meme.selectedImgId);
+    var cuurLine = meme.lines[meme.selectedLineIdx]
+    drawImg(img.url,cuurLine.txt)
+}
 
-console.log('txt');
+function drawImg(src,txt){
+    var img = new Image();
+    img.src = src;
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+        drawText(txt,234,50)
+        drawText(txt,240,481)
+    }
+}
+
+function drawText(text,x,y){
+    gCtx.strokeStyle = 'black'
+    gCtx.fillStyle = 'white'
+    gCtx.lineWidth = '2'
+    gCtx.font = '50px IMPACT'
+    gCtx.textAlign = 'center'
+    gCtx.fillText(text, x, y)
+    gCtx.strokeText(text, x, y)
+}
+
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,14 +1,33 @@
+'use strict';
 var gKeywords = {'happy':12,'funny':1};
 
-var gImgs = [{id:1,url:'imgs/meme-imgs/1.jpg',keywords:['happy']}];
+var gImgs = [{id:1,url:'/imgs/meme-imgs/1.jpg',keywords:['happy']},{id:2,url:'imgs/meme-imgs/2.jpg',keywords:['funny']}];
 
 var gMeme = {
     selectedImgId:1,
     selectedLineIdx:0,
-    line:[{
+    lines:[{
         txt:'I never eat Falafel',
         size:20,
         align:'left',
-        color:'red'
-    }]
-};
+        color:'white'
+    },
+    {
+        txt:'I never eat Hem',
+        size:10,
+        align:'center',
+        color:'white'
+    }
+]
+}
+
+function getMeme(){
+    return gMeme;
+}
+
+function findImgById(id){
+    var currImage = gImgs.find(img => img.id === id )
+    return currImage;
+}
+
+
